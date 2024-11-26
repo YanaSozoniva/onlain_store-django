f0a991605acad3f17d28573692fe594156a37d27
Yana Sozonova <ynakochneva@mail.ru>
2024-11-26 15:11:20 +0500

    добавление шаблонов home и contacts.html, настройка стилизации

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import home
+
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', )
+    path('', home, 'home')
 ]
