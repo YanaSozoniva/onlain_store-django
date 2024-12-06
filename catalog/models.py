5896ef3a26ca94202c9ffdfc2f8e8510cd4f1f7c
Yana Sozonova <ynakochneva@mail.ru>
2024-12-06 18:59:17 +0500

    Создание модели Контакты

--- catalog/models.py
+++ catalog/models.py
@@ -56,3 +56,22 @@ class Product(models.Model):
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
         ordering = ['name', 'category', 'price']
+
+
+class Contact(models.Model):
+    """Класс для создания модели(таблицы) контакты"""
+
+    country = models.CharField(max_length=50, verbose_name="Страна", help_text="Введите название страны")
+    address = models.CharField(max_length=150, verbose_name="Адрес", help_text="Введите Ваш адрес", black=True, null=True)
+    individual_tax_index = models.IntegerField(verbose_name='ИНН', blank=True, null=True)
+
+    def __str__(self):
+        """Метод для строкового отображения информации о контактах"""
+        return f'{self.country}, {self.address}'
+
+    class Meta:
+        """Данный класс используется для добавления метаданных к модели:
+        наименование модели в единственном и множественном числе"""
+
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
