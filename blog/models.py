2d863b0dd2482be8a90f93645ea2335cd3723685
Yana Sozonova <ynakochneva@mail.ru>
2025-01-02 09:29:51 +0500

    создание приложения blog, модели и миграции

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog (models.Model):
+    title = models.CharField(max_length=50, verbose_name="Заголовок", help_text="Введите название Заголовка")
+    content = models.TextField(verbose_name="Содержимое публикации", blank=True, null=True)
+    photo = models.ImageField(upload_to="photo_blog/", verbose_name="Изображение", blank=True, null=True)
+    created_at = models.DateField(verbose_name="Дата создания", auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+    views_counter = models.PositiveIntegerField(
+        verbose_name="Количество просмотров",
+        default=0
+    )
