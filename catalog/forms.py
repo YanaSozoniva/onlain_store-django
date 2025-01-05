d08fa1a4a17c387393a60307441beade8b7d2fa3
Yana Sozonova <ynakochneva@mail.ru>
2025-01-05 18:48:47 +0500

    Реализована валидация формы, чтобы проверять отсутствие запрещенных слов (в любом регистре) в полях name and description

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,8 @@
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 from catalog.models import Product
+
+EXCEPTION_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
 
 class ProductForm(ModelForm):
@@ -8,3 +11,17 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "description", "photo_product", "category", "price"]
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if name.lower() in EXCEPTION_WORDS:
+            raise ValidationError(f'Вы ввели запрещенное слово {name}! Исправляйтесь!!!')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+
+        for word in EXCEPTION_WORDS:
+            if word in description.lower():
+                raise ValidationError(f'Вы ввели запрещенное слово {word}! Исправляйтесь!!!')
+            return description
