f8d88a73c18f5d3f8363df2c99531835529de330
Yana Sozonova <ynakochneva@mail.ru>
2025-01-15 14:48:08 +0500

    настройка logout и закрытие доступа к страницам, связанным с управлением продуктами, для неавторизованных пользователей

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name='users/login.html'), name="login"),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
     # path('register/', UserCreateViews.as_view(), name='register'),
 
 ]
